6733eaa5a02e375d8c1b7413962b5c209b506a6d
l2jserverclient@gmail.com <l2jserverclient@gmail.com@11ce8ce8-224a-78c2-11ac-db8d4823ae60>
2012-06-22 21:56:58 +0000

    FCClient: updated DAO tests

--- FCClient/src/com/l2client/test/junit/DAOActionLoadCsvTest.java
+++ FCClient/src/com/l2client/test/junit/DAOActionLoadCsvTest.java
@@ -3,20 +3,21 @@ package com.l2client.test.junit;
 import junit.framework.TestCase;
 
 import com.l2client.dao.IDAO;
-import com.l2client.dao.derby.DatastoreDAO;
+import com.l2client.dao.csv.CSVDatastoreDAO;
 import com.l2client.gui.actions.BaseUsable;
 
 
-public class DAOActionLoadTest extends TestCase{
+public class DAOActionLoadCsvTest extends TestCase{
 	
 	public void testSelf() {
-		final IDAO dao = DatastoreDAO.get();
+		final IDAO dao = CSVDatastoreDAO.get();
 		dao.init();
 //		InputController.getInstance().setInGameInputHandler(new InputHandler());
 		BaseUsable[] ret = dao.loadAllActions();
-		dao.finit();
+		String name = dao.getNpcName(100);
 		assertTrue("Any actions returned", ret != null && ret.length > 0);
-
+		assertNotNull(name);
+		dao.finit();
 	}
 
 }
